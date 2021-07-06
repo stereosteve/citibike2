13f81c57c0c9de58d2c2467e3f88d19c3d471322
Steve Perkins <stereosteve@gmail.com>
2021-07-06 20:23:01 +0000

    vega stacked bar member / non-member

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,51 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import useSWR from 'swr'
+import * as aq from 'arquero';
 
 function sqlFetcher(query: string) {
   query += ' format JSON'
   return fetch(`/db?query=${query}`).then(r => r.json())
 }
 
+async function aqFetcher(query: string) {
+  const url = `/db?query=${query} format Arrow`
+  // @ts-ignore
+  const dt = await aq.loadArrow(url);
+  console.log(dt)
+  debugger
+  return {
+    data: [
+      {count: 99}
+    ]
+  }
+}
+
 function App() {
   const [count, setCount] = useState(0)
-  const {data, error} = useSWR('select count(*) as count from r2', sqlFetcher)
+  const {data, error} = useSWR('select birth_year, count(*) as count from r2 where birth_year > 0 group by birth_year', aqFetcher)
   if (error) return <div>error</div>
   if (!data) return <div>loading</div>
-  console.log('Data?', data)
+  // console.log('Data?', data)
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{data.data[0].count} Rides</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
